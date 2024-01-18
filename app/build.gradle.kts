41d0f75064c78b3d708cd9a2a29ebd278966e075
jaivi <jaivingadhiya12@gmail.com>
2024-01-18 11:29:04 +0530

    sdp & ssp library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,13 @@ android {
 
 dependencies {
 
+    //sdp
+    implementation("com.intuit.sdp:sdp-android:1.1.0")
+
+    //ssp
+    implementation("com.intuit.ssp:ssp-android:1.1.0")
+
+
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
