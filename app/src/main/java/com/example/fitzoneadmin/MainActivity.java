2e476473e4316abd409b38aed8da07dfe81a38b5
dharmik6 <dharmik.kacha.2526@outlook.com>
2024-02-16 16:50:20 +0530

    fix the branch problem

--- app/src/main/java/com/example/fitzoneadmin/MainActivity.java
+++ app/src/main/java/com/example/fitzoneadmin/MainActivity.java
@@ -10,7 +10,9 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
