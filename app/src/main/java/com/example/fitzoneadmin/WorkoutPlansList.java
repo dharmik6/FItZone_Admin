34320016068c3fdc87bfdae58a8063185e345928
jaivi <jaivingadhiya12@gmail.com>
2024-02-20 23:43:23 +0530

    add Workout & exercises home and workout plans  list and workout plan and edit workout and exercises  list and add images

---
+++ app/src/main/java/com/example/fitzoneadmin/WorkoutPlansList.java
@@ -0,0 +1,14 @@
+package com.example.fitzoneadmin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WorkoutPlansList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workout_plans_list);
+    }
+}
