82b592ea663a21566e4aea25e7852fe94b3e3329
dharmik6 <dharmik.kacha.2526@outlook.com>
2024-03-15 10:34:25 +0530

    purchases detail

--- app/src/main/java/com/example/fitzoneadmin/PurchaseDetails.java
+++ app/src/main/java/com/example/fitzoneadmin/PurchaseDetails.java
@@ -1,19 +1,98 @@
 package com.example.fitzoneadmin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatTextView;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
-public class PurchaseDetails extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
+public class PurchaseDetails extends AppCompatActivity {
+    FirebaseFirestore db;
+
+    AppCompatTextView paymentid,membername,packagename,packageduration,packagestatus,price,paidamount,paymentmethod,purchasedate,experience;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_purchase_details);
 
         ImageView backPress = findViewById(R.id.back);
+        paymentid = findViewById(R.id.pyment_id);
+        membername = findViewById(R.id.member_name);
+        packagename = findViewById(R.id.package_name);
+        packageduration = findViewById(R.id.package_duration);
+        packagestatus = findViewById(R.id.package_status);
+        price = findViewById(R.id.price);
+        paidamount = findViewById(R.id.paid_amount);
+        purchasedate = findViewById(R.id.purchase_date);
+
+
+
+        Intent intent = getIntent();
+        String id = intent.getStringExtra("id");
+        String name = intent.getStringExtra("name");
+        String packagE = intent.getStringExtra("package");
+
+        membername.setText(name);
+        packagename.setText(packagE);
+
+
+        db = FirebaseFirestore.getInstance();
+
+
+        db.collection("purchases").document(id).get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        // Dismiss ProgressDialog when data fetching is complete
+
+
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+                                // Retrieve additional data and set them to respective TextViews
+                                String duration = document.getString("duration");
+                                String paymentId = document.getString("paymentId");
+                                String pricE = document.getString("price");
+                                String purchasesDate = document.getString("purchasesDate");
+                                String status = document.getString("status");
+
+
+
+                                paymentid.setText(paymentId != null ? paymentId : "No date");
+                                packageduration.setText(duration != null ? duration : "No date");
+                                price.setText(pricE != null ? pricE : "No date");
+                                packagestatus.setText(status != null ? status : "No date");
+                                paidamount.setText(pricE != null ? pricE : "No date");
+                                purchasedate.setText(purchasesDate != null ? purchasesDate : "No payment id");
+
+                            } else {
+                                // Handle the case where the document does not exist
+                            }
+
+                        } else {
+                            // Handle exceptions while fetching data from Firestore
+                            FirebaseFirestoreException exception = (FirebaseFirestoreException) task.getException();
+                            // Log the exception or handle it as needed
+                        }
+                    }
+                });
+
+
+
+
         backPress.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/example/fitzoneadmin/PurchasesList.java
+++ app/src/main/java/com/example/fitzoneadmin/PurchasesList.java
@@ -1,12 +1,27 @@
 package com.example.fitzoneadmin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PurchasesList extends AppCompatActivity {
+
+    private RecyclerView purchasRecyclerView;
+    private PurchasAdapter adapter;
+    private List<MemberList> purchasLists;
+    private ProgressDialog progressDialog; // Declare ProgressDialog
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +35,57 @@ public class PurchasesList extends AppCompatActivity {
                 onBackPressed();
             }
         });
+
+        purchasRecyclerView = findViewById(R.id.purchas);
+        purchasRecyclerView.setHasFixedSize(true);
+        purchasRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        purchasLists = new ArrayList<>();
+        adapter = new PurchasAdapter(this, purchasLists);
+        purchasRecyclerView.setAdapter(adapter);
+
+        progressDialog = new ProgressDialog(this); // Initialize ProgressDialog
+        progressDialog.setMessage("Loading..."); // Set message for ProgressDialog
+        progressDialog.setCancelable(false); // Make ProgressDialog non-cancelable
+
+    }
+    protected void onResume() {
+        super.onResume();
+        loadDietData();
+    }
+    private void loadDietData() {
+        progressDialog.show(); // Show ProgressDialog before fetching data
+        purchasLists.clear(); // Clear the previous list
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("purchases")
+                .get()
+                .addOnSuccessListener(queryDocumentSnapshots -> {
+                    for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                        String userId = documentSnapshot.getString("userId");
+                        String packagename = documentSnapshot.getString("packageName");
+                        String id = documentSnapshot.getId();
+                        // Fetch user details using userId
+                        db.collection("users")
+                                .document(userId)
+                                .get()
+                                .addOnSuccessListener(userDocumentSnapshot -> {
+                                    // Retrieve user details
+                                    String name = userDocumentSnapshot.getString("name");
+                                    // Create a BookingItemList object with user details
+                                    MemberList purchasList = new MemberList(name, packagename ,id);
+                                    purchasLists.add(purchasList);
+                                    adapter.notifyDataSetChanged(); // Notify adapter about data changes
+                                    progressDialog.dismiss(); // Dismiss ProgressDialog after fetching data
+                                })
+                                .addOnFailureListener(e -> {
+                                    // Handle failure to fetch user details
+                                    progressDialog.dismiss(); // Dismiss ProgressDialog on failure
+                                });
+                    }
+                })
+                .addOnFailureListener(e -> {
+                    // Handle failure
+                    progressDialog.dismiss(); // Dismiss ProgressDialog on failure
+                });
     }
 }
